b36bf7ec5ccf8aa5962ffa2eb16f4716ccaf5e63
Daniel Rodriguez <danielrodriguezac@gmail.com>
2021-11-19 00:13:30 -0300

    feat(dev): All tests passed

--- controllers/trades.js
+++ controllers/trades.js
@@ -1,1 +1,46 @@
 
+const Trades = require('../models/trades')
+
+async function create(input) {
+  return await Trades.create({
+    ...input
+  })
+}
+
+async function update(id, input) {
+
+  const validStages = [ 1, 2, 3 ]
+  if (!validStages.includes(stage)) throw new Error(`Invalid : ${stage}`)
+
+  const targetTrade = await Trades.findByPk(id)
+  if (!targetTrade) {
+    throw new Error(`Could not find Board with id: ${id}`)
+  }
+  await targetTrade.update({
+    stage
+  })
+  return targetTrade
+}
+
+
+async function findByPk(id) {
+
+  const targetTrade = await Trades.findByPk(id)
+
+  if (!targetTrade) {
+    throw new Error('Trade not found')
+  }
+
+  return targetTrade
+}
+
+async function findAll() {
+  return Trades.findAll({ order: [ [ 'id', 'ASC' ] ], raw: true })
+}
+
+module.exports = {
+  create,
+  update,
+  findByPk,
+  findAll
+}
--- routes/trades.js
+++ routes/trades.js
@@ -1,5 +1,43 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+const controller = require('../controllers/trades')
 
 
-module.exports = router;
+router.post('/', async function(req, res, next) {
+  
+  const newTrade = await controller.create({
+    type: req.body.type,
+    user_id: req.body.user_id,
+    symbol: req.body.symbol,
+    shares: req.body.shares,
+    price: req.body.price,
+    timestamp: req.body.timestamp
+  })
+  res.status(201).send(newTrade.get({ plain: true }))
+  next()
+})
+
+router.get('/', async function(_, res, next) {
+  console.log(await controller.findAll())
+  res.status(200).send(await controller.findAll())
+  next()
+})
+
+router.get('/:id', async function(req, res, next) {
+  try {
+    res.status(200).send(await controller.findByPk(req.params.id))
+  } catch (e) {
+    res.status(404).send('ID not found')
+  } finally {
+    next()
+  }
+})
+
+router.route('/:id').put(nope).patch(nope).delete(nope)
+
+async function nope(_, res, next) {
+  res.status(405).end()
+  next()
+}
+module.exports = router
